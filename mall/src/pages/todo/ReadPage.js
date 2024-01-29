5afbaf83bd9e2e299047e536193da93fdfbe22b9
mfocto <skrgus0140@naver.com>
2024-01-29 22:18:51 +0900

    first commit

---
+++ mall/src/pages/todo/ReadPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ReadPage(props) {
+    return (
+        <div className={'text-3xl'}>
+            todo Read Page
+        </div>
+    );
+}
+
+export default ReadPage;
